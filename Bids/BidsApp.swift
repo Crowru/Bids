b5b76fa149e0b79714b31c8b86ae735d32bf6a51
Ruslan <ruslanusmanov7779@gmail.com>
2024-02-07 19:53:49 +0300

    Initial Commit

---
+++ Bids/BidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BidsApp.swift
+//  Bids
+//
+//  Created by Руслан  on 07.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
